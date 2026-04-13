55377cccece2ca1ebe30f0aa4df7b72a36b209e6
Klykaylo V. I. <klykaylova@gmail.com>
2026-04-13 17:22:31 +0300

    fix review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,7 +2,7 @@ class ReviewsController < ApplicationController
   def create
     @product = Product.find(params[:product_id])
     @review = @product.reviews.new(review_params)
-    @review.user_id = 1
+    @review.user = current_user
 
     if @review.save
       redirect_to @product, notice: "Ваш відгук додано!"
